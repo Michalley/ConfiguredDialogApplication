94b42ef50fc1d4bd4a74cd207ee3b78d3caf39d5
Michalley <ml5877@bs.amalnet.k12.il>
2018-11-17 17:00:01 +0200

    2nd commit

--- app/src/main/java/com/example/michal/configureddialogapplication/MainActivity.java
+++ app/src/main/java/com/example/michal/configureddialogapplication/MainActivity.java
@@ -5,8 +5,11 @@ import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -76,5 +79,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void btn3(View view) {
         ly.setBackgroundColor(Color.WHITE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Credits");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this,"Application Of Michal Leybovich",Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
     }
 }
